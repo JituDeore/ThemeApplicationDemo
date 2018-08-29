f5466cd82c254ccff88215dfd925b80ef1ac776d
Jitendra.deore <jitendra.deore@verse.in>
2018-08-29 11:39:53 +0530

    Added new UIViewController objectiveC class to show how to use themeManger in legacy code

---
+++ ThemeApplication/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  ThemeApplication
+//
+//  Created by Jitendra Deore on 29/08/18.
+//  Copyright © 2018 Jitendra Deore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
